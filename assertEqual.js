2c728634958d240ad192d1fb3b6e4ec8a819b86b
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-09-13 21:04:06 +0000

    finish the assertEqual functionality

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  const happy = '😄';
+  const sad = '😥';
+  let response = '';
+  if (actual === expected) {
+    response = happy + ' Assertion Passed: ' + actual + ' === ' + expected;
+  } else {
+    response = sad + ' Assertion Failed: ' + actual + ' !== ' + expected;
+  }
+  return response;
+};
+
+// TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+
