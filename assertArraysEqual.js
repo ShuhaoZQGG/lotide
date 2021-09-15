0f5d0e30a13b36ad18005d638da5bf91a0e7483d
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-09-15 13:32:00 +0000

    refactor code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,11 @@
+import {eqArrays} from './module.js';
 const assertArraysEqual = function(actual, expected) {
   const happy = '😄';
   const sad = '😥';
-  let answer = `${happy} Assertion Passed: ${actual} === ${expected}`;
-  if (actual.length !== expected.length) {
-    answer = `${sad} Assertion Failed: ${actual} !== ${expected}`;
-  } else {
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]) {
-        answer = `${sad} Assertion Failed: ${actual} !== ${expected}`;
-      }
-    }
-  }
-  console.log(answer);
-  return answer;
+
+  eqArrays
+    ? console.log(`${happy} Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`${sad} Assertion Failed: ${actual} !== ${expected}`);
 };
 
 
