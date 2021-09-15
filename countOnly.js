97c5f3fabe6bb5fe33ffd33d2b8a30ed1f1785d6
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-09-15 18:34:55 +0000

    complete the countOnly challenge

---
+++ countOnly.js
@@ -0,0 +1,35 @@
+import { assertEqual } from "./module.js";
+
+const countOnly = function(allItems, itemsToCount) {
+  const object = new Object();
+
+  for (const item of allItems) {
+    if (itemsToCount[item] === true) {
+      if (object[item]) {
+        object[item] ++;
+      } else {
+        object[item] = 1;
+      }
+    }
+  }
+
+  return object;
+};
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+console.log(result1);
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
