efdc47a586f8ef4b7b4704557ce3354c238114ed
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-09-15 18:35:39 +0000

    update module

--- module.js
+++ module.js
@@ -69,3 +69,18 @@ export const middle = function(array) {
   console.log(middle);
   return middle;
 };
+
+export const countOnly = function(allItems, itemsToCount){
+  const object = new Object();
+
+  for (const item of allItems){
+    if (itemsToCount[item] === true){
+      if (object[item]){
+        object[item] ++ ;
+      } else {
+        object[item] = 1;
+      }
+    }
+  }
+  return object;
+}
