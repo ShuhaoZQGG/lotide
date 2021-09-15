79567b7345f04c9a85c6266e9f3187629992bfe2
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-09-15 21:10:23 +0000

    refactor the code

--- letterPositions.js
+++ letterPositions.js
@@ -6,23 +6,12 @@ const letterPositions = function(sentence) {
   sentence = sentence.toLowerCase();
   for (let i = 0; i < sentence.length; i++) {
     const currentChar = sentence[i];
-    if (currentChar === ' ') {
-      continue;
-    }
-    /*
-    if (results[sentence[i]]) {
-      results[sentence[i]].push(i);
-    } else {
-      results[sentence[i]] = [i];
-    }
-  */if (!results[currentChar]) {
-      results[currentChar] = [];
-    }
+    if (currentChar === ' ') continue;
+
+    if (!results[currentChar]) results[currentChar] = [];
+
     results[currentChar].push(i);
   }
-
-  // delete results[' '];
-  
 
   console.log(results);
   return results;
