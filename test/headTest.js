5dbf2ade035ee5403fb696d63b732543fd075d3e
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-09-20 19:47:34 -0400

    refactor code by using mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,26 @@
 const head = require('../head');
 const assertEqual = require('../assertEqual');
-console.log(head([1,12,9]));
-console.log(head([]));
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+const assert = require('chai').assert;
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("return 5 will be false for ['5', 4, 3, 2, 1]", () => {
+    assert.notStrictEqual(head(['5', 4, 3, 2, 1]), 5);
+  });
+
+  it(`return "Hello" for ["Hello", "Lighthouse", "Labs"]`, () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it(`return "hello" will be false for ["Hello", "Lighthouse", "Labs"]`, () => {
+    assert.notStrictEqual(head(["Hello", "Lighthouse", "Labs"]), "hello");
+  });
+});
+
