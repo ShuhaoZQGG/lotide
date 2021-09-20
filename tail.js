1062f496aab1c5fd4c2d8c5aeede08f54f37c50b
Shuhao Zhang <shuhao.zhang@mail.mcgill.ca>
2021-09-20 18:43:26 -0400

    refactor the code by creating test and modular code

--- tail.js
+++ tail.js
@@ -1,5 +1,3 @@
-import { assertEqual } from "./module.js";
-
 const tail = function(list) {
   let answer;
   if (list === []) {
@@ -8,18 +6,8 @@ const tail = function(list) {
     return answer;
   } else {
     answer = list.slice(1);
-    console.log(answer);
     return answer;
   }
 };
 
-tail([1,2,3,4]);
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-assertEqual(result, ["Lighthouse","Labs"]);
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,13 @@
+const assertEqual = require('../assertEqual');
+const tail = require('../tail')
+
+console.log(tail([1,2,3,4]));
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+assertEqual(result, ["Lighthouse","Labs"]);
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words)); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
